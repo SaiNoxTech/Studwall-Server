52f960c45efaecec9707ca151543708eac9f372a
shivam123425 <shivam123425@gmail.com>
2019-05-25 18:26:39 +0530

    Fix not breaking out of loop bug, switch to async/await

--- middleware/validateOrderItems.js
+++ middleware/validateOrderItems.js
@@ -1,6 +1,6 @@
 const Item = require("../models/Item");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   // Validate all the items in req.body.items[] and populate them
   const promiseArr = [];
   let promise;
@@ -8,14 +8,16 @@ module.exports = (req, res, next) => {
     promise = Item.findOne({ itemId: item.id });
     promiseArr.push(promise);
   });
-  Promise.all(promiseArr).then(resultItemsArr => {
+  try {
+    const resultItemsArr = await Promise.all(promiseArr);
     console.log(resultItemsArr);
-    resultItemsArr.forEach(item => {
+    for (let i = 0; i < resultItemsArr.length; i++) {
+      let item = resultItemsArr[i];
       // item is falsy thus id of an element in req.body.items[] does not exists.
       if (!item) {
         const error = new Error("One or more items does not exists!");
         error.statusCode = 404;
-        next(error);
+        return next(error);
       } else {
         // Find the fetched(resultItemsArr) item's itemId in req.body.items[]
         const foundItem = req.body.items.find(bodyItem => {
@@ -24,11 +26,13 @@ module.exports = (req, res, next) => {
         // Add the qty attribute from it to the item in resultItemsArr
         item.qty = foundItem.qty;
       }
-    });
+    }
     // Put resultItemsArr in req.items.
     // It will contain an item with its price and qty.
     // To be used in calculateTotal helper.
     req.items = resultItemsArr;
     next();
-  });
+  } catch (error) {
+    next(error);
+  }
 };
