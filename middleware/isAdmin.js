eda800cc5b5839ff4f533c447fb3ee34aeeae89f
shivam123425 <shivam123425@gmail.com>
2019-05-25 18:40:58 +0530

    Verify auth token first

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,8 +1,13 @@
 const PrimeVendor = require("../models/PrimeVendor");
+const { verifyToken } = require("../helpers/jwtToken");
 
 module.exports = async (req, res, next) => {
   // Find if logged in user is Prime Vendor
   try {
+    const payload = verifyToken(req.headers.authorization);
+    if (payload.error) {
+      return next(payload.errorMessage);
+    }
     const primeVendor = await PrimeVendor.findOne({
       vendorId: req.user.vendorId
     });
