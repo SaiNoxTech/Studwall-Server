f3bb28cdd229c9f4fa54b6328c3352814f78e1b8
shivam123425 <shivam123425@gmail.com>
2019-06-05 23:03:27 +0530

    Add route to generate new one time pass for vendor

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -45,3 +45,46 @@ exports.postAddVendor = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.postGenerateNewVendorPass = async (req, res, next) => {
+  try {
+    const vendorObj = {};
+    if (req.body.email) {
+      vendorObj.email = req.body.email;
+    }
+    if (req.body.phone) {
+      vendorObj.phone = req.body.phone;
+    }
+
+    // Find a vendor based on provided email and password.
+    const foundVendor = await Vendor.findOne({
+      $or: [{ email: vendorObj.email }, { phone: vendorObj.phone }]
+    });
+    if (!foundVendor) {
+      const error = new Error("No vendor found with given credentials.");
+      error.statusCode = 404;
+      return next(error);
+    }
+    // Generate new password
+    const generatedPassword = generateRandomToken(6);
+    bcrypt.genSalt(12, async (err, salt) => {
+      if (err) {
+        throw err;
+      }
+      bcrypt.hash(generatedPassword, salt, null, async (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        foundVendor.password = hash;
+        await foundVendor.save();
+        // Send mail to the vendor with the created password
+        res.json({
+          success: true,
+          password: generatedPassword
+        });
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.post("/add-vendor", adminController.postAddVendor);
+router.post("/generatepass", adminController.postGenerateNewVendorPass);
 
 module.exports = router;
