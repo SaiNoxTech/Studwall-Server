2ea7e0f39909306f3618d3f19853e3e8f6348935
shivam123425 <shivam123425@gmail.com>
2019-06-07 23:44:50 +0530

    Add isVendor middleware

--- middleware/typeOfUser.js
+++ middleware/typeOfUser.js
@@ -1,13 +1,20 @@
-exports.isStudent = (req, res, next) => {
+function checkForUser(typeOfUser,req,next){
   if (!req.user) {
     const error = new Error("User not logged in.");
     return next(error);
   }
-  if (req.user.userType === "Student") {
+  if (req.user.userType === typeOfUser) {
     next();
   } else {
     const error = new Error("Only student can add money");
     error.statusCode = 403;
     next(error);
   }
+}
+
+exports.isStudent = (req, res, next) => {
+  return checkForUser("Student", req,next);
+};
+exports.isVendor = (req, res, next) => {
+  return checkForUser("Vendor", req, next);
 };
