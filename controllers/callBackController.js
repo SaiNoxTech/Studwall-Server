03dedb06336cc11c7124dc8ac0a470f41a136637
shivam123425 <shivam123425@gmail.com>
2019-06-15 16:16:17 +0530

    Minor fix

--- controllers/callBackController.js
+++ controllers/callBackController.js
@@ -36,7 +36,7 @@ exports.postHandleTransaction = async (req, res, next) => {
         error.statusCode = 500;
         return next(error);
       }
-      if (foundOrder.STATUS != "PENDING") {
+      if (foundOrder.STATUS !== "PENDING") {
         const error = new Error("Txn has already being completed!");
         error.statusCode = 403;
         return next(error);
